54f08287365de1c03a4b725b3e9441097d17aa78
XiaoYang <tlylz99@live.cn>
2022-01-01 22:24:44 +0800

    Upgrade to .NET 6.0

--- src/Host/HelperExtensions.cs
+++ src/Host/HelperExtensions.cs
@@ -1,8 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 using System;
 using System.IO.Abstractions;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 
 namespace JetHub
 {
@@ -18,11 +28,83 @@ namespace JetHub
             byte[] etag = BitConverter.GetBytes(sysInfo.LastWriteTimeUtc.Ticks);
 
             var result = new StringBuilder(2 + etag.Length * 2);
-            result.Append("\"");
+            result.Append('"');
             foreach (byte b in etag) result.AppendFormat("{0:x2}", b);
-            result.Append("\"");
+            result.Append('"');
 
             return new EntityTagHeaderValue(result.ToString());
+        }
+
+        public static IEndpointConventionBuilder MapFileServer(this IEndpointRouteBuilder endpoints, string prefix, IFileProvider fileProvider)
+        {
+            prefix = prefix.TrimEnd('/');
+
+            FileServerOptions options = new()
+            {
+                EnableDirectoryBrowsing = true,
+                EnableDefaultFiles = false,
+                RequestPath = prefix,
+                FileProvider = fileProvider,
+            };
+
+            return endpoints.Map(
+                prefix + "/{**slug}",
+                endpoints.CreateApplicationBuilder()
+                    .UseMiddleware<FileServerV2Middleware>(Options.Create(options))
+                    .Build());
+        }
+
+        private class FileServerV2Middleware
+        {
+            private readonly IWebHostEnvironment _environment;
+            private readonly IOptions<FileServerOptions> _options;
+            private readonly ILoggerFactory _loggerFactory;
+            private readonly HtmlEncoder _htmlEncoder;
+
+            public FileServerV2Middleware(
+                RequestDelegate next,
+                IWebHostEnvironment environment,
+                ILoggerFactory loggerFactory,
+                HtmlEncoder htmlEncoder,
+                IOptions<FileServerOptions> options)
+            {
+                this._htmlEncoder = HtmlEncoder.Default;
+                this._loggerFactory = loggerFactory;
+                this._environment = environment;
+                this._options = options;
+            }
+
+            public Task Invoke(HttpContext context)
+            {
+                context.SetEndpoint(null);
+                return this.InvokeDirectoryBrowser(context);
+            }
+
+            private Task InvokeDirectoryBrowser(HttpContext context)
+            {
+                return new DirectoryBrowserMiddleware(
+                    this.InvokeStaticFile,
+                    this._environment,
+                    this._htmlEncoder,
+                    Options.Create(this._options.Value.DirectoryBrowserOptions))
+                    .Invoke(context);
+            }
+
+            private Task InvokeStaticFile(HttpContext context)
+            {
+                return new StaticFileMiddleware(
+                    this.InvokeEopl,
+                    this._environment,
+                    Options.Create(this._options.Value.StaticFileOptions),
+                    this._loggerFactory)
+                    .Invoke(context);
+            }
+
+            private Task InvokeEopl(HttpContext context)
+            {
+                context.Response.StatusCode = 404;
+                return Task.CompletedTask;
+            }
         }
     }
 }
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -1,16 +1,80 @@
+using JetHub.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System;
+using System.Reflection;
 
 namespace JetHub
 {
     public sealed class Program
     {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>());
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDirectoryBrowser();
+
+            builder.Services.Configure<GlobalOptions>(options =>
+            {
+                options.HostName = System.Net.Dns.GetHostName();
+                var info = typeof(Program).Assembly.GetCustomAttribute<GitVersionAttribute>();
+                options.Branch = info.Branch;
+                options.CommitId = info.Version;
+                options.Version = typeof(Program).Assembly.GetName().Version?.ToString() ?? "0.0.0.0";
+            });
+
+            builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<GlobalOptions>>().Value);
+            builder.Services.AddSignalR();
+
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                builder.Services.AddSingleton<ISystemInfo, ProcfsSystemInfo>();
+
+                builder.Services.AddSingleton<AptPackageService>();
+                builder.Services.AddSingleton<IPackageService>(sp => sp.GetRequiredService<AptPackageService>());
+                builder.Services.AddSingleton<IHostedService>(sp => sp.GetRequiredService<AptPackageService>());
+
+                builder.Services.AddSingleton<DfFreeStorageInfo>();
+                builder.Services.AddSingleton<IStorageInfo>(sp => sp.GetRequiredService<DfFreeStorageInfo>());
+                builder.Services.AddSingleton<IHostedService>(sp => sp.GetRequiredService<DfFreeStorageInfo>());
+
+                builder.Services.AddSingleton<OneShotGlobalInfo>();
+                builder.Services.AddSingleton<IGlobalInfo>(sp => sp.GetRequiredService<OneShotGlobalInfo>());
+                builder.Services.AddSingleton<IHostedService>(sp => sp.GetRequiredService<OneShotGlobalInfo>());
+            }
+            else
+            {
+                builder.Services.AddSingleton<ISystemInfo, FakeSystemInfo>();
+                builder.Services.AddSingleton<IPackageService, FakePackageService>();
+                builder.Services.AddSingleton<IStorageInfo, FakeStorageInfo>();
+                builder.Services.AddSingleton<IGlobalInfo, FakeGlobalInfo>();
+            }
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app.MapHub<LogHub>("/api/log-stream");
+            app.MapFileServer("/judgings", new PhysicalFileProvider(app.Services.GetRequiredService<IGlobalInfo>().VfsRoot));
+
+            app.Run();
+        }
     }
 }
