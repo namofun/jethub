ff46294af6ce262137835d3874fe56aab261ae13
XiaoYang <tlylz99@live.cn>
2022-03-05 13:17:39 +0800

    Rename to InfoController

---
+++ src/Host/Controllers/InfoController.cs
@@ -0,0 +1,49 @@
+﻿using JetHub.Models;
+using JetHub.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JetHub.Controllers
+{
+    [Route("/api/[controller]/[action]")]
+    public class InfoController : ControllerBase
+    {
+        private readonly IHostSystem hostSystem;
+
+        public InfoController(IHostSystem hostSystem)
+        {
+            this.hostSystem = hostSystem;
+        }
+
+        [HttpGet]
+        public Task<SystemInformation> System()
+        {
+            return hostSystem.GetSystemInformationAsync();
+        }
+
+        [HttpGet]
+        public Task<List<InstalledPackage>> Dpkg([FromQuery] bool isChroot)
+        {
+            return hostSystem.GetInstalledPackagesAsync(isChroot ? "/chroot/domjudge/" : "/");
+        }
+
+        [HttpGet]
+        public Task<List<CpuInformation>> Cpu()
+        {
+            return hostSystem.GetCpuInformationAsync();
+        }
+
+        [HttpGet]
+        public Task<KernelInformation> Kernel()
+        {
+            return hostSystem.GetKernelInformationAsync();
+        }
+
+        [HttpGet]
+        public Task<List<DriveInformation>> Disk([FromQuery] bool fixedOnly = true)
+        {
+            return hostSystem.GetDriveInformationAsync(fixedOnly);
+        }
+    }
+}
