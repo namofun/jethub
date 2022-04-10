9121611da7c1afad0d22a6e6a148dac50804ce10
XiaoYang <tlylz99@live.cn>
2022-04-10 13:28:22 +0800

    Add SatelliteSite Host for sample.

---
+++ src/SatelliteSite/HostModule.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace SatelliteSite
+{
+    public class HostModule : AbstractModule
+    {
+        public override string Area => string.Empty;
+
+        public override void Initialize()
+        {
+        }
+
+        public override void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureIdentityAdvanced(options =>
+            {
+                options.ShortenedClaimName = true;
+            });
+        }
+
+        public override void RegisterEndpoints(IEndpointBuilder endpoints)
+        {
+            endpoints.MapRequestDelegate("/", context =>
+            {
+                context.Response.Redirect("/dashboard");
+                return Task.CompletedTask;
+            })
+            .WithDisplayName("Home Page");
+        }
+    }
+}
---
+++ src/SatelliteSite/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using SatelliteSite.IdentityModule.Entities;
+
+namespace SatelliteSite
+{
+    public class Program
+    {
+        public static IHost Current { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            Current = CreateHostBuilder(args).Build();
+            Current.AutoMigrate<DefaultContext>();
+            Current.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .MarkDomain<Program>()
+                .AddModule<IdentityModule.IdentityModule<User, Role, DefaultContext>>()
+                .AddModule<JobsModule.JobsModule<User, DefaultContext>>()
+                .AddModule<HostModule>()
+                .AddDatabase<DefaultContext>((c, b) => b.UseSqlServer(c.GetConnectionString("UserDbConnection"), b => b.UseBulk()))
+                .ConfigureSubstrateDefaults<DefaultContext>();
+    }
+}
