9503000c3836440b7bf70a2126a569d2f7ccce16
XiaoYang <tlylz99@live.cn>
2022-08-26 22:54:56 +0800

    Integrate PSWS into JetHub

---
+++ src/Host/Controllers/PowerShellController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Threading.Tasks;
+using Xylab.Management.Automation;
+
+namespace JetHub.Controllers
+{
+    [Route("/api/pwsh/[action]")]
+    public class PowerShellController : ControllerBase
+    {
+        [HttpPost("{cmdletName}")]
+        public async Task<IActionResult> Cmdlet([FromRoute] string cmdletName)
+        {
+            if (!Request.ContentType!.StartsWith("application/xml"))
+            {
+                Response.Headers.Add("X-PSWS-Error", "Invalid request type.");
+                return new BadRequestResult();
+            }
+
+            Hashtable boundParameters;
+            using (StreamReader sr = new(Request.Body))
+            {
+                string value = await sr.ReadToEndAsync();
+                PSObject inputObject = (PSObject)PSSerializer.Deserialize(value);
+                if (inputObject.ImmediateBaseObject is not Hashtable)
+                {
+                    Response.Headers.Add("X-PSWS-Error", "Invalid $PSBoundParameters.");
+                }
+
+                boundParameters = (Hashtable)inputObject.ImmediateBaseObject;
+            }
+
+            using Runspace runspace = Bundle.CreateRunspace();
+            using PowerShell pwsh = PowerShell.Create(runspace);
+            pwsh.AddCommand(cmdletName).AddParameters(boundParameters);
+            var result = await pwsh.InvokeAsync();
+
+            return new ContentResult
+            {
+                Content = PSSerializer.Serialize(result),
+                ContentType = "application/xml"
+            };
+        }
+    }
+}
